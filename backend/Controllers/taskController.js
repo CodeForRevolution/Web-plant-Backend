428820e600e3f2df904ff44091e62f373384ba5b
shakirBunna <shakir973019@gmail.com>
2024-02-27 00:46:07 +0530

    deployment

--- backend/Controllers/taskController.js
+++ backend/Controllers/taskController.js
@@ -114,10 +114,7 @@ exports.deleteTask = catchAsyncError(async (req, res, next) => {
 
 module.exports.completed = catchAsyncError(async (req, res, next) => {
   const date = new Date();
-  console.log("Date", date);
-
   const task = await Task.findOne({ _id: req.params.id });
-  console.log("your task", task);
   task.isCompleted = true;
   task.completedDate = new Date();
   await task.save();
